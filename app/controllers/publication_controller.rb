4ce7bb40762adb6b726d3f2daf7cec28adde21cb
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2013-10-24 13:14:35 +0200

    Adding actions to Controller.

--- app/controllers/publication_controller.rb
+++ app/controllers/publication_controller.rb
@@ -3,6 +3,7 @@ class PublicationController < ApplicationController
 		@query = params[:q]
 		if @query
 			render json: SolrPublication::get_publications(@query)
+
 		end
 	end
 
@@ -10,5 +11,24 @@ class PublicationController < ApplicationController
 		@publicationId = params[:id]
 		@jsondata = Publication::testj
 		render json: @jsondata
+	end
+
+	def create
+		status = "200 OK"
+		puts "============="
+		puts params
+		puts "============="
+		render :nothing => true, :status => status
+	end
+
+	def update
+		status = "200 OK"
+		render :nothing => true, :status => status
+	end
+
+	def destroy
+
+		status = "403"
+		render :nothing => true, :status => status
 	end
 end
