0576d0e7f43b13e626ff46c14df517e2a1ac4718
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2013-10-25 12:39:52 +0200

    Put in the basic implementation of the CRUD methods of the Publication model.

--- app/controllers/publication_controller.rb
+++ app/controllers/publication_controller.rb
@@ -1,4 +1,6 @@
 class PublicationController < ApplicationController
+  
+  # GET /publications/
   def index
     query = params[:q]
     params[:start] ? start = params[:start] : start = 0
@@ -9,27 +11,63 @@ class PublicationController < ApplicationController
     end
   end
 
+  # GET /publications/{id}
   def show
-    @publicationId = params[:id]
-    @jsondata = Publication::testj
-    render json: @jsondata
+    publication_id = params[:id]
+    publication = Publication.find_by_id(publication_id)
+    if publication
+      render :json => publication.to_json, :status => "200 OK"
+    else
+      render :nothing => true, :status => "404 Not Found"
+    end
   end
 
+  # POST /publications/
   def create
-    status = "200 OK"
-    puts "============="
-    puts params
-    puts "============="
+    publication = Publication.new(params[:publication])
+    if publication.save
+      headers['location'] = "/publications/#{publication.id}"
+      status = "201 Created"
+    else
+      status = "501 Internal Server Error"
+    end
     render :nothing => true, :status => status
   end
 
+  # PUT /publications/{id}
   def update
-    status = "200 OK"
+    status = "501 Internal Server Error"
+    publication_id = params[:id]
+    publication = Publication.find_by_id(publication_id)
+
+    if publication
+      publication.update_attributes(params[:publication])
+      # TODO Put in some more code to check attributes in JSON
+      # We dont want to change each and every attribute like created_by
+      if publication.save
+        status = "200 OK"
+      else
+        status = "400 Bad Request"
+      end
+    else
+      status = "404 Not Found"
+    end
     render :nothing => true, :status => status
   end
 
+  # DELETE /publications/{id}
   def destroy
-    status = "403"
+    status = "501 Internal Server Error"
+    publication_id = params[:id]
+    publication = Publication.find_by_id(publication_id)
+    
+    if publication
+      if publication.destroy
+        status = "200 OK"
+      end
+    else
+      status = "404 Not Found"
+    end
     render :nothing => true, :status => status
   end
   
