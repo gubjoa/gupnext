435fc9bce07ff19e4c47846198002e2ec5c36c21
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2013-10-25 08:01:32 +0200

    Added missing belongs_to relation to some of the models.

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -6,6 +6,8 @@ class Publication < ActiveRecord::Base
   has_many :people, through: :addresses
   has_many :publication_data
   has_many :publication_meta_data
+  belongs_to :publication_type
+  belongs_to :language
 
   def self.getz
     {
