5a5fa3fa1f23dddaa539c1f917a8d5080ee64665
Per Larsson <per.larsson@ub.gu.se>
2013-10-28 15:38:11 +0100

    Change the fulltext link field representation in JSON output. Changed SOLR source to 'new intra'

--- app/models/solr_publication.rb
+++ app/models/solr_publication.rb
@@ -3,8 +3,10 @@ require 'net/http'
 
 # Model to SOLR index, to perform search and get operations
 class SolrPublication < ActiveRecord::Base
-  SOLR_BASE_ULR = 'http://gup.ub.gu.se/'
-  SOLR_PUBLICATION_INDEX = 'solr/publications/scigloo'
+#  SOLR_BASE_ULR = 'http://gup.ub.gu.se/'
+#  SOLR_PUBLICATION_INDEX = 'solr/publications/scigloo'
+  SOLR_BASE_ULR = 'http://130.241.35.208:8080/'
+  SOLR_PUBLICATION_INDEX = 'solr/scigloo/select'
 
   def self.get_publications(query, start, rows)
     uri = URI(SOLR_BASE_ULR + SOLR_PUBLICATION_INDEX)
@@ -17,6 +19,15 @@ class SolrPublication < ActiveRecord::Base
     puts uri.query
     res = Net::HTTP.get_response(uri) 
     parsed = JSON.parse(res.body)
+    parsed['response']['docs'].each do |doc|
+    # Change the fulltext field representation
+    fulltext_field = doc['fulltext_url']
+      if fulltext_field
+        new_json = handle_fulltext_links(fulltext_field)
+        doc.merge!(:download_links => new_json)
+        doc.delete('fulltext_url')
+      end
+    end
     # return the complete SOLR response
     return parsed
   end
@@ -31,7 +42,41 @@ class SolrPublication < ActiveRecord::Base
     res = Net::HTTP.get_response(uri) 
     parsed = JSON.parse(res.body)
     # remove the SOLR "frame"
-    return parsed['response']['docs'][0]
+    doc = parsed['response']['docs'][0]
+    # Change the fulltext field representation
+    fulltext_field = doc['fulltext_url']
+    if fulltext_field
+      new_json = handle_fulltext_links(fulltext_field)
+      doc.merge!(:download_links => new_json)
+      doc.delete('fulltext_url')
+    end
+    return doc
   end
 
+  def self.handle_fulltext_links(field)
+    # Create new field fragment
+    new_field = []
+    field.each do |f|
+      if f.start_with?("1:")
+        type = 1
+        description = "Fulltext"
+        oa = false
+      elsif f.start_with?("2:")
+        type = 2
+        description = "Abstract"
+        oa = false
+      elsif f.start_with?("3:")
+        type = 3
+        description = "Fulltext Lokal OA"
+        oa = true
+      else # unknown type, do nothing?
+        type = f[0]
+        description = "Unknown"
+        puts "Unknown fulltext type: " + ft
+      end
+      # Add the link data to the new field
+      new_field << {:url => f[2..-1], :type => type, :description => description, :OA => oa}
+    end
+    return new_field
+  end
 end
